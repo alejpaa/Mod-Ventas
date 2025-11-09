5867b9a6cc227fa21c62d95893cec013347e7803
Gerardo <gerarleonce-0703@outlook.es>
2025-11-08 22:50:41 -0500

    Se indica en la navegación donde está actualmente el usuario

--- fronted_venta/src/LayoutPrincipal.tsx
+++ fronted_venta/src/LayoutPrincipal.tsx
@@ -1,6 +1,6 @@
 // src/components/LayoutPrincipal.tsx
 
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, NavLink } from 'react-router-dom';
 import { useRole } from './contexts/RoleContext';
 
 export function LayoutPrincipal() {
@@ -26,17 +26,23 @@ export function LayoutPrincipal() {
     <div className="flex h-screen bg-gray-100">
 
       {/* 1. BARRA LATERAL (SIEMPRE FIJA)  */}
-  <aside className="w-64 bg-[#3C83F6] text-white p-6 shadow-xl">
-  <h1 className="text-2xl font-bold mb-6 text-center">Venta</h1>
-
-        {/* Navegación */}
+      <aside className="w-64 bg-[#3C83F6] text-white p-6 shadow-xl">
+        <h1 className="text-2xl font-bold mb-6 text-center">Venta</h1>
+            {/* Navegación */}
         <nav>
           <ul className="space-y-3">
             {nav.map((item) => (
               <li key={item.to}>
-                <Link to={item.to} className="block py-2 px-3 rounded hover:bg-[#2e6ee0]">
+                <NavLink
+                  to={item.to}
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'block py-2 px-3 rounded bg-[#1F2937]' // color azul oscuro para "activo"
+                      : 'block py-2 px-3 rounded hover:bg-[#2e6ee0]' // color normal/inactivo
+                  }
+                >
                   {item.label}
-                </Link>
+                </NavLink>
               </li>
             ))}
           </ul>
