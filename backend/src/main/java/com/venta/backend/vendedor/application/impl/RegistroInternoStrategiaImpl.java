28419837a4af5258ebad57b3191c205e957f4e36
Gerardo <gerarleonce-0703@outlook.es>
2025-12-03 20:51:00 -0500

    Implementación del patrón observer en la strategy createinter

--- backend/src/main/java/com/venta/backend/vendedor/application/impl/RegistroInternoStrategiaImpl.java
+++ backend/src/main/java/com/venta/backend/vendedor/application/impl/RegistroInternoStrategiaImpl.java
@@ -1,19 +1,16 @@
 package com.venta.backend.vendedor.application.impl;
 
+import com.venta.backend.vendedor.application.adaptadores.IAdaptadorEmpleado;
 import com.venta.backend.vendedor.application.dto.request.RegistroVendedorRequest;
 import com.venta.backend.vendedor.application.estrategias.IRegistroVendedorStrategia;
 import com.venta.backend.vendedor.application.exceptions.RecursoNoEncontradoException;
 import com.venta.backend.vendedor.application.exceptions.RegistroVendedorException;
 import com.venta.backend.vendedor.entities.Vendedor;
-import com.venta.backend.vendedor.enums.DocumentType;
-import com.venta.backend.vendedor.enums.SellerStatus;
 import com.venta.backend.vendedor.infraestructura.clientes.IClienteRRHH;
 import com.venta.backend.vendedor.infraestructura.clientes.dto.EmpleadoRRHHDTO;
 import com.venta.backend.vendedor.infraestructura.repository.VendedorRepositorio;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor // Para agregar las dependencias
@@ -21,6 +18,7 @@ public class RegistroInternoStrategiaImpl implements IRegistroVendedorStrategia 
 
     private final VendedorRepositorio vendedorRepositorio;
     private final IClienteRRHH clienteRRHH;
+    private final IAdaptadorEmpleado adaptadorEmpleado;
 
     @Override
     public void validateData(RegistroVendedorRequest request) {
@@ -39,20 +37,12 @@ public class RegistroInternoStrategiaImpl implements IRegistroVendedorStrategia 
                         "Empleado no encontrado en RRHH. Verifique el DNI."
                 ));
 
-        return Vendedor.builder()
-                .dni(empleadoRRHH.getDni())
-                .firstName(empleadoRRHH.getFirstName())
-                .lastName(empleadoRRHH.getLastName())
-                .email(empleadoRRHH.getEmail())
-                .phoneNumber(empleadoRRHH.getPhoneNumber())
-                .address(empleadoRRHH.getAddress())
-                .sellerType(request.getSellerType())
-                .sellerStatus(SellerStatus.ACTIVE)
-                .registrationDate(LocalDate.now())
-                .employeeRrhhId(empleadoRRHH.getEmployeeId())
-                .documentType(DocumentType.DNI)
-                .bankAccount(request.getBankAccount())
-                .bankName(request.getBankName())
-                .build();
+        Vendedor newVendedor = adaptadorEmpleado.adaptar(empleadoRRHH);
+
+        newVendedor.setSellerType(request.getSellerType());
+        newVendedor.setBankAccount(request.getBankAccount());
+        newVendedor.setBankName(request.getBankName());
+
+        return newVendedor;
     }
 }
